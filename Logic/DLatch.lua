27fabb7c8c6a6831da5df0cfa02270cca33c606e
coogy29-create <coogy29@gmail.com>
2026-08-06 15:32:29 +0900

    Update DLatch.lua

--- Logic/DLatch.lua
+++ Logic/DLatch.lua
@@ -1,50 +1,52 @@
-local Context=getgenv().BABFT_CALCULATOR
+local Context = getgenv().BABFT_CALCULATOR
 
-local Gates=Context.Modules.Gates
-local Wiring=Context.Modules.Wiring
-local Components=Context.Modules.Components
+local Gates = Context.Modules.Gates
+local Wiring = Context.Modules.Wiring
 
-local DLatch={}
+local DLatch = {}
 
-local function P(origin,x,z)
-	return origin*CFrame.new(x,0,z)
+local function P(origin, x, y, z)
+	return origin * CFrame.new(
+		x or 0,
+		y or 0,
+		z or 0
+	)
 end
 
-function DLatch.Build(name,origin)
-
-	local notD=Gates.Not(
-		name.."_NOT_D",
-		P(origin,0,4)
+function DLatch.Build(name, origin)
+	local notD = Gates.Not(
+		name .. "_NOT_D",
+		P(origin, 0, 0, 8)
 	)
 
-	local setGate=Gates.And(
-		name.."_SET",
-		P(origin,6,0)
+	local setGate = Gates.And(
+		name .. "_SET",
+		P(origin, 12, 0, 0)
 	)
 
-	local resetGate=Gates.And(
-		name.."_RESET",
-		P(origin,6,8)
+	local resetGate = Gates.And(
+		name .. "_RESET",
+		P(origin, 12, 0, 12)
 	)
 
-	local orQ=Gates.Or(
-		name.."_OR_Q",
-		P(origin,12,0)
+	local qOr = Gates.Or(
+		name .. "_Q_OR",
+		P(origin, 28, 0, 0)
 	)
 
-	local notQ=Gates.Not(
-		name.."_Q",
-		P(origin,18,0)
+	local qNot = Gates.Not(
+		name .. "_Q",
+		P(origin, 40, 0, 0)
 	)
 
-	local orQB=Gates.Or(
-		name.."_OR_QB",
-		P(origin,12,8)
+	local qbOr = Gates.Or(
+		name .. "_QB_OR",
+		P(origin, 28, 0, 12)
 	)
 
-	local notQB=Gates.Not(
-		name.."_QB",
-		P(origin,18,8)
+	local qbNot = Gates.Not(
+		name .. "_QB",
+		P(origin, 40, 0, 12)
 	)
 
 	Wiring.Connect(
@@ -53,53 +55,69 @@ function DLatch.Build(name,origin)
 	)
 
 	Wiring.Connect(
-		setGate,
-		orQB
-	)
-
-	Wiring.Connect(
 		resetGate,
-		orQ
+		qOr
 	)
 
 	Wiring.Connect(
-		orQ,
-		notQ
+		setGate,
+		qbOr
 	)
 
 	Wiring.Connect(
-		orQB,
-		notQB
+		qOr,
+		qNot
 	)
 
 	Wiring.Connect(
-		notQ,
-		orQB
+		qbOr,
+		qbNot
 	)
 
 	Wiring.Connect(
-		notQB,
-		orQ
+		qNot,
+		qbOr
 	)
 
-	return{
-		Q=notQ,
-		QB=notQB,
+	Wiring.Connect(
+		qbNot,
+		qOr
+	)
 
-		DataTargets={
-			notD,
+	local function connectData(source)
+		Wiring.Connect(
+			source,
+			notD
+		)
+
+		Wiring.Connect(
+			source,
 			setGate
-		},
+		)
+	end
 
-		EnableTargets={
-			setGate,
+	local function connectEnable(source)
+		Wiring.Connect(
+			source,
+			setGate
+		)
+
+		Wiring.Connect(
+			source,
 			resetGate
-		}
-	}
+		)
+	end
 
+	return {
+		Q = qNot,
+		QB = qbNot,
+		Set = setGate,
+		Reset = resetGate,
+		ConnectData = connectData,
+		ConnectEnable = connectEnable
+	}
 end
 
-Components.DLatch=DLatch.Build
-Context.Modules.DLatch=DLatch
+Context.Modules.DLatch = DLatch
 
 return DLatch
