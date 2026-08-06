515bf84f48d2fb37fe5e604b46412cb3abe5d636
coogy29-create <coogy29@gmail.com>
2026-08-06 19:38:09 +0900

    Update DisplayDriver.lua

--- Display/DisplayDriver.lua
+++ Display/DisplayDriver.lua
@@ -117,14 +117,33 @@ local function P(origin, x, y, z)
 	)
 end
 
-local function buildDecoder(name, origin, inputBus)
+local function L(origin, index, depth)
+	return Config.Layout.GetLayeredCFrame(
+		origin,
+		index,
+		depth or 0
+	)
+end
+
+local function getPixelIndex(row, column)
+	return (row - 1) * 5 + (column - 1)
+end
+
+local function buildDecoder(
+	name,
+	logicOrigin,
+	inputBus
+)
 	local inverted = {}
 	local values = {}
 
 	for bit = 0, 3 do
 		inverted[bit] = Gates.Not(
 			name .. "_NOT_" .. bit,
-			P(origin, bit * 8, 0, 0)
+			L(
+				logicOrigin,
+				bit
+			)
 		)
 
 		Wiring.Connect(
@@ -136,17 +155,17 @@ local function buildDecoder(name, origin, inputBus)
 	for value = 0, 9 do
 		local detector = Gates.And(
 			name .. "_VALUE_" .. value,
-			P(
-				origin,
-				(value % 5) * 10,
-				0,
-				12 + math.floor(value / 5) * 10
+			L(
+				logicOrigin,
+				4 + value
 			)
 		)
 
 		for bit = 0, 3 do
 			local enabled =
-				math.floor(value / (2 ^ bit)) % 2 == 1
+				math.floor(
+					value / (2 ^ bit)
+				) % 2 == 1
 
 			Wiring.Connect(
 				enabled
@@ -165,20 +184,37 @@ local function buildDecoder(name, origin, inputBus)
 	}
 end
 
-function DisplayDriver.Build(name, origin, digitCount)
+function DisplayDriver.Build(
+	name,
+	origin,
+	digitCount
+)
 	local self = {}
 
 	digitCount = digitCount or 5
+
+	local displayConfig =
+		Config.Display or {}
+
+	local pixelSpacing =
+		displayConfig.PixelSpacing or 3
+
+	local digitSpacing =
+		displayConfig.DigitSpacing or 19
 
 	self.Digits = {}
 	self.Decoders = {}
 	self.Pixels = {}
 
-	function self.ConnectDigit(digitIndex, inputBus)
+	function self.ConnectDigit(
+		digitIndex,
+		inputBus
+	)
 		assert(
 			digitIndex >= 1
 				and digitIndex <= digitCount,
-			"잘못된 자릿수: " .. tostring(digitIndex)
+			"잘못된 자릿수: "
+				.. tostring(digitIndex)
 		)
 
 		assert(
@@ -189,80 +225,116 @@ function DisplayDriver.Build(name, origin, digitCount)
 		for bit = 0, 3 do
 			assert(
 				inputBus[bit],
-				"BCD 입력 비트 누락: " .. bit
+				"BCD 입력 비트 누락: "
+					.. tostring(bit)
 			)
 		end
 
 		local digitOrigin = P(
 			origin,
-			(digitIndex - 1) * 96,
+			(digitIndex - 1)
+				* digitSpacing,
 			0,
 			0
 		)
 
+		local logicOrigin = P(
+			digitOrigin,
+			0,
+			0,
+			-8
+		)
+
 		local decoder = buildDecoder(
-			name .. "_DIGIT_" .. digitIndex,
-			P(digitOrigin, 0, 0, -42),
+			name
+				.. "_DIGIT_"
+				.. digitIndex,
+			logicOrigin,
 			inputBus
 		)
 
-		self.Decoders[digitIndex] = decoder
+		self.Decoders[digitIndex] =
+			decoder
+
 		self.Digits[digitIndex] = {}
 		self.Pixels[digitIndex] = {}
 
 		for row = 1, 7 do
-			self.Pixels[digitIndex][row] = {}
+			self.Pixels[digitIndex][row] =
+				{}
 
 			for column = 1, 5 do
-				local pixelName = string.format(
-					"%s_D%d_R%d_C%d",
-					name,
-					digitIndex,
-					row,
-					column
-				)
-
-				local pixelX = column * 5
-				local pixelY = (8 - row) * 5
-
-				local display = Builder.PlaceNamedBlock(
-					pixelName,
-					"DisplayBlock",
-					P(
-						digitOrigin,
-						pixelX,
-						pixelY,
-						0
+				local pixelName =
+					string.format(
+						"%s_D%d_R%d_C%d",
+						name,
+						digitIndex,
+						row,
+						column
 					)
-				)
 
-				local whiteOutput = Gates.Or(
-					pixelName .. "_WHITE",
-					P(
-						digitOrigin,
-						pixelX,
-						pixelY,
-						-14
+				local pixelIndex =
+					getPixelIndex(
+						row,
+						column
 					)
-				)
 
-				local blackOutput = Gates.Not(
-					pixelName .. "_BLACK",
-					P(
-						digitOrigin,
-						pixelX,
-						pixelY,
-						-24
+				local pixelX =
+					(column - 1)
+					* pixelSpacing
+
+				local pixelY =
+					(7 - row)
+					* pixelSpacing
+
+				local display =
+					Builder.PlaceNamedBlock(
+						pixelName,
+						"DisplayBlock",
+						P(
+							digitOrigin,
+							pixelX,
+							pixelY,
+							0
+						)
 					)
-				)
+
+				local whiteOutput =
+					Gates.Or(
+						pixelName
+							.. "_WHITE",
+						L(
+							logicOrigin,
+							14
+								+ pixelIndex
+									* 2
+						)
+					)
+
+				local blackOutput =
+					Gates.Not(
+						pixelName
+							.. "_BLACK",
+						L(
+							logicOrigin,
+							15
+								+ pixelIndex
+									* 2
+						)
+					)
 
 				for value = 0, 9 do
-					if FONT[value][row]:sub(
-						column,
-						column
-					) == "1" then
+					local enabled =
+						FONT[value][row]:sub(
+							column,
+							column
+						) == "1"
+
+					if enabled then
 						Wiring.Connect(
-							decoder.Values[value],
+							decoder.Values[
+								value
+							],
 							whiteOutput
 						)
 					end
@@ -285,19 +357,25 @@ function DisplayDriver.Build(name, origin, digitCount)
 
 				Context:QueuePaint({
 					Object = whiteOutput,
-					Color = Config.Colors.White
+					Color =
+						Config.Colors.White
 				})
 
 				Context:QueuePaint({
 					Object = blackOutput,
-					Color = Config.Colors.Black
+					Color =
+						Config.Colors.Black
 				})
 
 				self.Digits[digitIndex][
-					#self.Digits[digitIndex] + 1
+					#self.Digits[
+						digitIndex
+					] + 1
 				] = display
 
-				self.Pixels[digitIndex][row][column] = {
+				self.Pixels[
+					digitIndex
+				][row][column] = {
 					Display = display,
 					White = whiteOutput,
 					Black = blackOutput
@@ -309,6 +387,7 @@ function DisplayDriver.Build(name, origin, digitCount)
 	return self
 end
 
-Context.Modules.DisplayDriver = DisplayDriver
+Context.Modules.DisplayDriver =
+	DisplayDriver
 
 return DisplayDriver
