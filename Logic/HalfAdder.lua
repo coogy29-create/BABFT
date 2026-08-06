8ebaac0cdac426d2ab316f87b8b5ac712745a2dd
coogy29-create <coogy29@gmail.com>
2026-08-06 15:47:17 +0900

    Update HalfAdder.lua

--- Logic/HalfAdder.lua
+++ Logic/HalfAdder.lua
@@ -1,79 +1,49 @@
-local Context=getgenv().BABFT_CALCULATOR
+local Context = getgenv().BABFT_CALCULATOR
 
-local Gates=Context.Modules.Gates
-local Wiring=Context.Modules.Wiring
+local Gates = Context.Modules.Gates
+local Wiring = Context.Modules.Wiring
 
-local HalfAdder={}
+local HalfAdder = {}
 
 local function P(origin,x,y,z)
-	return origin*CFrame.new(
-		x or 0,
-		y or 0,
-		z or 0
-	)
+	return origin * CFrame.new(x or 0,y or 0,z or 0)
 end
 
 function HalfAdder.Build(name,origin)
 
-	local xorGate=Gates.Xor(
+	local xor = Gates.Xor(
 		name.."_XOR",
 		P(origin,0,0,0)
 	)
 
-	local andGate=Gates.And(
+	local andGate = Gates.And(
 		name.."_AND",
 		P(origin,0,0,8)
 	)
 
-	local inputA={}
-	local inputB={}
+	local self = {}
 
-	function HalfAdder.ConnectA(source)
+	function self.ConnectA(source)
 
-		inputA[1]=source
-
-		Wiring.Connect(
-			source,
-			xorGate
-		)
-
-		Wiring.Connect(
-			source,
-			andGate
-		)
+		Wiring.Connect(source,xor)
+		Wiring.Connect(source,andGate)
 
 	end
 
-	function HalfAdder.ConnectB(source)
+	function self.ConnectB(source)
 
-		inputB[1]=source
-
-		Wiring.Connect(
-			source,
-			xorGate
-		)
-
-		Wiring.Connect(
-			source,
-			andGate
-		)
+		Wiring.Connect(source,xor)
+		Wiring.Connect(source,andGate)
 
 	end
 
-	return{
+	self.Sum = xor
+	self.Carry = andGate
 
-		Sum=xorGate,
-
-		Carry=andGate,
-
-		ConnectA=HalfAdder.ConnectA,
-
-		ConnectB=HalfAdder.ConnectB
-
-	}
+	return self
 
 end
 
-Context.Modules.HalfAdder=HalfAdder
+Context.Modules.HalfAdder = HalfAdder
 
 return HalfAdder
