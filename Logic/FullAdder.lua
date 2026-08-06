4f47215073273ffcd3c18d4f7b0759a879ca8227
coogy29-create <coogy29@gmail.com>
2026-08-06 15:29:29 +0900

    Update FullAdder.lua

--- Logic/FullAdder.lua
+++ Logic/FullAdder.lua
@@ -1,62 +1,80 @@
 local Context=getgenv().BABFT_CALCULATOR
 
-local Components=Context.Modules.Components
+local HalfAdder=Context.Modules.HalfAdder
 local Gates=Context.Modules.Gates
 local Wiring=Context.Modules.Wiring
 
 local FullAdder={}
 
-local function P(origin,x,z)
-
-	return CFrame.new(
-		origin.Position+
-		Vector3.new(x,0,z)
+local function P(origin,x,y,z)
+	return origin*CFrame.new(
+		x or 0,
+		y or 0,
+		z or 0
 	)
-
 end
 
 function FullAdder.Build(name,origin)
 
-	local HA1=Components.HalfAdder(
+	local ha1=HalfAdder.Build(
 		name.."_HA1",
-		P(origin,0,0)
+		P(origin,0,0,0)
 	)
 
-	local HA2=Components.HalfAdder(
+	local ha2=HalfAdder.Build(
 		name.."_HA2",
-		P(origin,8,0)
+		P(origin,24,0,0)
 	)
 
-	local OR=Gates.Or(
-		name.."_OR",
-		P(origin,16,2)
+	local carryOr=Gates.Or(
+		name.."_CARRY_OR",
+		P(origin,48,0,4)
+	)
+
+	function FullAdder.ConnectA(source)
+
+		ha1.ConnectA(source)
+
+	end
+
+	function FullAdder.ConnectB(source)
+
+		ha1.ConnectB(source)
+
+	end
+
+	function FullAdder.ConnectCarryIn(source)
+
+		ha2.ConnectB(source)
+
+	end
+
+	Wiring.Connect(
+		ha1.Sum,
+		ha2.Sum
 	)
 
 	Wiring.Connect(
-		HA1.Sum,
-		name.."_HA2_A"
+		ha1.Carry,
+		carryOr
 	)
 
 	Wiring.Connect(
-		name.."_Cin",
-		name.."_HA2_B"
-	)
-
-	Wiring.Connect(
-		HA1.Carry,
-		name.."_OR"
-	)
-
-	Wiring.Connect(
-		HA2.Carry,
-		name.."_OR"
+		ha2.Carry,
+		carryOr
 	)
 
 	return{
 
-		Sum=HA2.Sum,
+		Sum=ha2.Sum,
 
-		Carry=OR
+		Carry=carryOr,
+
+		ConnectA=FullAdder.ConnectA,
+
+		ConnectB=FullAdder.ConnectB,
+
+		ConnectCarryIn=FullAdder.ConnectCarryIn
 
 	}
 
